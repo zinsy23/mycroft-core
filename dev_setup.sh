e93563c87c8c44c1198f4069e9f582b02c2a4c42
zinsy23 <jdzinsy@gmail.com>
2025-08-14 16:27:44 -0500

    Updated setup script to use python virtual environment from version on system (keeping latest RPi in mind)

--- dev_setup.sh
+++ dev_setup.sh
@@ -16,7 +16,7 @@ echo "Setting up Mycroft for offline/local use..."
 # Create virtual environment
 if [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
-    python3.11 -m venv .venv
+    python3 -m venv .venv
 fi
 
 # Activate virtual environment
